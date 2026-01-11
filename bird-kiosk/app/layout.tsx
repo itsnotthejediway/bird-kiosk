19961b49fc62512a2e22c6b2cf09adfa1a22827e
dsr0010 <114751234+ASCTE-DEV@users.noreply.github.com>
2026-01-11 13:53:44 -0600

    asdf

--- bird-kiosk/app/layout.tsx
+++ bird-kiosk/app/layout.tsx
@@ -8,14 +8,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
       </head>
-      <body className="bg-background text-foreground">{children}</body>
+      <body className="min-h-dvh bg-background text-foreground antialiased">
+        {children}
+      </body>
     </html>
   );
 }
